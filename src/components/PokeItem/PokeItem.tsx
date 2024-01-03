84c2d0b074762b7a32241379e15e956c6788c949
Christian <bournecreative@gmail.com>
2024-01-03 15:15:58 -0600

    create component Poke Item. Represents one list item returned from pokeAPI. This components is a list item that is clickable and will open a new page with details specific to this pokemon. Incorporate Skeleton component

---
+++ src/components/PokeItem/PokeItem.tsx
@@ -0,0 +1,54 @@
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { Skeleton } from "./Skeleton/Skeleton";
+import styles from "./PokeItem.module.css";
+
+export const PokeItem = ({ name, url }: { name: string; url: string }) => {
+  const { data, isLoading } = useQuery(
+    ["pokemon-detail", name],
+    async () => {
+      return await fetch(url).then((res) => res.json());
+    },
+    {
+      staleTime: 600_000,
+    }
+  );
+  return (
+    <>
+      {!isLoading ? (
+        <Link to={`/details/${data.name}`} className={styles["poke-item"]}>
+          <div>
+            <div className={styles["poke-name"]}>
+              <div>#{data.id}.</div>
+              <strong>{data.name}</strong>
+            </div>
+            <img
+              src={data.sprites.other.dream_world.front_default}
+              alt={data.name}
+              width="96"
+              height="96"
+            />
+            <div className={styles["poke-stats"]}>
+              <div className={styles["poke-stat"]}>
+                <strong>Weight: </strong>
+                {data.weight}lbs
+              </div>
+              <div className={styles["poke-stat"]}>
+                <strong>Height: </strong>
+                {data.height}'
+              </div>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className={styles["skeleton-item"]}>
+          <Skeleton style={{ width: 96, height: 96 }} />
+          <div className={styles["skeleton-content"]}>
+            <Skeleton style={{ width: 120, height: 21 }} />
+            <Skeleton style={{ width: 256, height: 21 }} />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
